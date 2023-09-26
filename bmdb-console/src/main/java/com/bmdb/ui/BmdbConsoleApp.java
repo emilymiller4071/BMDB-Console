56b15300ee00fdba5ea3d06761ec6dd4e509f8d7
Emily Miller <emilyrauch3@gmail.com>
2023-09-26 11:54:12 -0400

    iteration adds movies into an ArrayList

--- bmdb-console/src/main/java/com/bmdb/ui/BmdbConsoleApp.java
+++ bmdb-console/src/main/java/com/bmdb/ui/BmdbConsoleApp.java
@@ -3,28 +3,39 @@ package com.bmdb.ui;
 import com.bmdb.console.Console;
 import com.bmdb.business.Movie;
 
+import java.util.ArrayList;
+
+
 public class BmdbConsoleApp {
     public static void main(String[] args) {
+
+        ArrayList<Movie> movies = new ArrayList<>();
 
         Console console = new Console();
         displayWelcome();
 
         String wantsToContinue = "y";
         while (wantsToContinue.equalsIgnoreCase("y")) {
-            Movie movie = new Movie();
+            Movie newMovie = new Movie();
             console.println("Enter data for a movie:\n");
 
             int id = console.getInt("ID: ");
-            movie.setId(id);
+            newMovie.setId(id);
             String title = console.getString("Title: ");
-            movie.setTitle(title);
+            newMovie.setTitle(title);
             int year = console.getInt("Year: ");
-            movie.setYear(year);
+            newMovie.setYear(year);
             String rating = console.getString("Rating: ");
-            movie.setRating(rating);
+            newMovie.setRating(rating);
             String director = console.getString("Director: ");
-            movie.setDirector(director);
-            console.println(movie.toString());
+            newMovie.setDirector(director);
+            console.println(newMovie.toString());
+            console.println();
+            movies.add(newMovie);
+            displayMovies(movies);
+
+            System.out.println();
+
             console.println();
             wantsToContinue = console.getString("Try again? (y/n): ", "y", "n");
         }
@@ -32,6 +43,19 @@ public class BmdbConsoleApp {
         displayGoodbye();
     }
 
+    private static void displayMovies(ArrayList<Movie> movies) {
+        Console.println("\nMovie List:");
+        Console.println("ID     TITLE               YEAR    RATING  DIRECTOR");
+        Console.println("=====  =================== ====    ======  ================");
+        for (Movie movie : movies) {
+            Console.println(
+                    String.format("%-6d %-20s %-6d %-8s %-20s",
+                            movie.getId(), movie.getTitle(), movie.getYear(),
+                            movie.getRating().toUpperCase(), movie.getDirector())
+            );
+        }
+        Console.println(); // Add a newline after displaying movies
+    }
     private static void displayWelcome() {
         Console.println("Welcome to BMDB Console Application!\n");
     }
